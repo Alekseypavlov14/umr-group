d47ab8c65fc0be526b587fb837ba88dfc5fa373b
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-06-15 11:20:48 +0300

    first section in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import { Container } from '../../components/Container/Container'
+import { TextBlock } from '../../components/TextBlock/TextBlock'
+import image from './../../assets/images/translator.png'
 import styles from './Home.module.css'
 
 interface HomeProps {}
@@ -8,7 +10,24 @@ const Home: FC<HomeProps> = () => {
   return (
     <div className={styles.Home}>
       <Container>
-        Home
+
+        <section className={styles.Section}>
+          <div className={styles.Content}>
+            <TextBlock
+              title={<>Ми - <span>UMR Group</span></>}
+              content="Ми допомагаємо Вам знаходити перекладачів з російської на 
+              румунську мову. Наші спеціалісти вирішують проблеми, з якими зустрічаються 
+              громадяни України.
+              "
+            />
+          </div>
+          <img 
+            className={styles.Image} 
+            src={image} 
+            alt='translator' 
+          />
+        </section>
+
       </Container>
     </div>
   )

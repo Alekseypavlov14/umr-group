20406a74e95bbb167802a40f72b7cba6d160c6ed
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-06-15 11:53:41 +0300

    remove button from home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,4 @@
 import { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Button } from '../../components/Button/Button'
 import { Container } from '../../components/Container/Container'
 import { TextBlock } from '../../components/TextBlock/TextBlock'
 import image from './../../assets/images/translator.png'
@@ -9,8 +7,6 @@ import styles from './Home.module.css'
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
-  const navigate = useNavigate()
-
   return (
     <div className={styles.Home}>
       <Container>
@@ -23,14 +19,6 @@ const Home: FC<HomeProps> = () => {
               румунську мову. Наші спеціалісти вирішують проблеми, з якими зустрічаються 
               громадяни України.
               "
-            />
-            <Button 
-              href='/order'
-              children='Order'
-              onClick={(e) => {
-                e.preventDefault()
-                navigate('/order')
-              }}
             />
           </div>
           <img 

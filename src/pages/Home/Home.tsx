860b88ff3939a1b21fa0dd504ba00888a7dcc938
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-06-15 12:13:26 +0300

    sections in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { Container } from '../../components/Container/Container'
 import { TextBlock } from '../../components/TextBlock/TextBlock'
+import cn from 'classnames'
 import image from './../../assets/images/translator.png'
 import styles from './Home.module.css'
 
@@ -9,26 +10,39 @@ interface HomeProps {}
 const Home: FC<HomeProps> = () => {
   return (
     <div className={styles.Home}>
-      <Container>
-
-        <section className={styles.Section}>
-          <div className={styles.Content}>
-            <TextBlock
-              title={<>Ми - <span>UMR&nbsp;Group</span></>}
-              content="Ми допомагаємо Вам знаходити перекладачів з російської на 
-              румунську мову. Наші спеціалісти вирішують проблеми, з якими зустрічаються 
-              громадяни України.
-              "
+      <section className={styles.Section}>
+        <Container>
+          <div className={styles.Section__Content}>
+            <div className={styles.Content}>
+              <TextBlock
+                title={<>Ми - <span>UMR&nbsp;Group</span></>}
+                content="Ми допомагаємо Вам знаходити перекладачів з російської на 
+                румунську мову. Наші спеціалісти вирішують проблеми, з якими зустрічаються 
+                громадяни України."
+              />
+            </div>
+            <img 
+              className={styles.Image} 
+              src={image} 
+              alt='translator' 
             />
           </div>
-          <img 
-            className={styles.Image} 
-            src={image} 
-            alt='translator' 
-          />
-        </section>
+        </Container>
+      </section>
 
-      </Container>
+      <section className={cn(styles.Section, styles.Section_without_image)}>
+        <Container>
+          <div className={styles.Section__Content}>
+            <div className={styles.Content}>
+              <TextBlock
+                title='Які питання ми можемо допомогти вирішити?'
+                content="Наша команда пропонує два вида послуг перекладача: 
+                по телефону та на місці."
+              />
+            </div>
+          </div>
+        </Container>
+      </section>
     </div>
   )
 }
